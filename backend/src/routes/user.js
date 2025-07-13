5bcbf6fa55f334632e632b687455642f334e648d
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-13 15:13:58 +0530

    add joi  validate schema  in route

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,9 +2,11 @@ import express from "express";
 const router=express.Router();
 import { registerUser,loginUser } from "../controller/user.js";
 import wrapAsync from "../utils/wrapAsync.js"
+import validate from "../middleware/userMiddleware.js";
+import {userSchemaReigster,userSchemaLogin } from "../joiSchema/user.js"
 
 
-router.post("/register",wrapAsync(registerUser))
-router.post("/login",wrapAsync(loginUser));
+router.post("/register",validate(userSchemaReigster),wrapAsync(registerUser))
+router.post("/login",validate(userSchemaLogin),wrapAsync(loginUser));
 
 export default router;
