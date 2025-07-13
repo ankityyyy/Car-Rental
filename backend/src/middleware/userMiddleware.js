2900ad3cb684b19c6996c4d8ddb4331f582248c2
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-13 15:18:24 +0530

    add validate middleware function

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -2,5 +2,16 @@ import ExpressError from "../utils/ExpressError";
 import { StatusCodes } from "http-status-codes";
 
 const validate=(schema)=>{
-     return 
+
+     return (req,res,next)=>{
+          let {error}=schema.validate(req.body);
+          if(error){
+              const errMsg=error.map((err)=>err.message).join(", ");
+              throw new ExpressError(errMsg,StatusCodes.BAD_REQUEST);
+          }
+          next()
+
+     }
 }
+
+export default validate;
