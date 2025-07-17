39b9bd4648aa67ef1f804ae3fd07df76a58a0fe6
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-17 17:08:24 +0530

    import editCarData from controller and add deletecar in routes from controller

--- backend/src/routes/carRoutes.js
+++ backend/src/routes/carRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getAllCar,getCarById,addCar} from "../controller/carController.js"
+import { getAllCar,getCarById,addCar,editCarData,deleteCar} from "../controller/carController.js"
 import wrapAsync from "../utils/wrapAsync.js"
 import {roleMiddleware} from '../middleware/rolebasedMiddleWare.js';
 import validate from "../middleware/userMiddleware.js";
@@ -11,5 +11,6 @@ router.get("/",wrapAsync(getAllCar));
 router.get("/:id",wrapAsync(getCarById));
 router.post('/new',validate(carAddSchema),roleMiddleware("admin", "owner"),wrapAsync(addCar));
 router.put("/:id",wrapAsync(editCarData));
+router.delete("/:id",wrapAsync(deleteCar))
 
 export default router;
