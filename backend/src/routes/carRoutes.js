959cec9015b38ffbe1949cc34a0379dd35233d9e
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-18 17:11:42 +0530

    add rollMiddleware or updateCarSchema

--- backend/src/routes/carRoutes.js
+++ backend/src/routes/carRoutes.js
@@ -4,13 +4,13 @@ import { getAllCar,getCarById,addCar,editCarData,deleteCar} from "../controller/
 import wrapAsync from "../utils/wrapAsync.js"
 import {roleMiddleware} from '../middleware/rolebasedMiddleWare.js';
 import validate from "../middleware/userMiddleware.js";
-import {carAddSchema} from "../joiSchema/carJoiSchema.js"
+import {carAddSchema,updateCarSchema} from "../joiSchema/carJoiSchema.js"
 
 
 router.get("/",wrapAsync(getAllCar));
 router.get("/:id",wrapAsync(getCarById));
 router.post('/new',validate(carAddSchema),roleMiddleware("admin", "owner"),wrapAsync(addCar));
-router.put("/:id",wrapAsync(editCarData));
-router.delete("/:id",wrapAsync(deleteCar))
+router.put("/:id",validate(updateCarSchema),roleMiddleware("admin", "owner"),wrapAsync(editCarData));
+router.delete("/:id",roleMiddleware("admin", "owner"),wrapAsync(deleteCar))
 
 export default router;
