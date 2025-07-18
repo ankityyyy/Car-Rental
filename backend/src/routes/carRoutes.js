222c72f23b5b4c84bf23bc9c54154b82bc67245d
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-18 17:29:33 +0530

    add isLogin Middleware in all routes

--- backend/src/routes/carRoutes.js
+++ backend/src/routes/carRoutes.js
@@ -3,14 +3,20 @@ const router = express.Router();
 import { getAllCar,getCarById,addCar,editCarData,deleteCar} from "../controller/carController.js"
 import wrapAsync from "../utils/wrapAsync.js"
 import {roleMiddleware} from '../middleware/rolebasedMiddleWare.js';
+import {isLogin} from "../middleware/isLogin.js"
 import validate from "../middleware/userMiddleware.js";
 import {carAddSchema,updateCarSchema} from "../joiSchema/carJoiSchema.js"
 
 
-router.get("/",wrapAsync(getAllCar));
-router.get("/:id",wrapAsync(getCarById));
-router.post('/new',validate(carAddSchema),roleMiddleware("admin", "owner"),wrapAsync(addCar));
-router.put("/:id",validate(updateCarSchema),roleMiddleware("admin", "owner"),wrapAsync(editCarData));
-router.delete("/:id",roleMiddleware("admin", "owner"),wrapAsync(deleteCar))
+router.route("/")
+.get(isLogin,wrapAsync(getAllCar));
+router.route('/new')
+.post(isLogin,validate(carAddSchema),roleMiddleware("admin", "owner"),wrapAsync(addCar));
+router.route("/:id")
+.get(isLogin,wrapAsync(getCarById))
+.patch(isLogin,validate(updateCarSchema),roleMiddleware("admin", "owner"),wrapAsync(editCarData))
+.delete(isLogin,roleMiddleware("admin", "owner"),wrapAsync(deleteCar))
+
+
 
 export default router;
