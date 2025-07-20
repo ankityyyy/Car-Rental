58fec269a3a3fa093021d2760a0f57e08883af3f
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-20 15:40:33 +0530

    add middleware and joi schema

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import {getReviewByCarId} from "../controller/reviewController.js"
+import {getReviewByCarId,addReview,updateReview,deleteReview} from "../controller/reviewController.js"
 import wrapAsync from "../utils/wrapAsync.js"
-import {roleMiddleware} from '../middleware/rolebasedMiddleWare.js';
+import {checkCar, checkUser} from '../middleware/reviewMiddleware.js';
 import validate from "../middleware/userMiddleware.js";
+import {reviewJoiSchema,updateReviewJoiSchema} from "../joiSchema/reviewJoiSchema.js"
 
 
-router.get("/:carId",wrapAsync(getReviewByCarId));
+router.get("/:id",checkCar,wrapAsync(getReviewByCarId));
+router.post("/:id",checkCar,checkUser,validate(reviewJoiSchema),wrapAsync(addReview))
+router.patch("/:id", checkUser,validate(updateReviewJoiSchema),wrapAsync(updateReview))
+router.delete("/:id",checkUser,wrapAsync(deleteReview))
+
+export default router;
