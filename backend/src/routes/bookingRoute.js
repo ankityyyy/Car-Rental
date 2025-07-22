a79289eecf249c98af8a07bdcf3816a8b15b499b
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-22 19:28:03 +0530

    add booking routes

--- backend/src/routes/bookingRoute.js
+++ backend/src/routes/bookingRoute.js
@@ -1,12 +1,23 @@
 import express from 'express';
 const router=express.Router();
-import { allBooking} from "../controller/carController.js"
+import { allBooking,allBookofCar,getBookingOfToday,getUserBookings, addUserBooking,updateBooking,deleteBooking } from "../controller/bookingController.js"
 import wrapAsync from "../utils/wrapAsync.js";
 import {roleMiddleware} from '../middleware/rolebasedMiddleWare.js';
 import {isLogin} from "../middleware/isLogin.js"
 import validate from "../middleware/userMiddleware.js";
+import {checkCar, checkUser} from '../middleware/reviewMiddleware.js';
 
 
-router.get('/',isLogin,roleMiddleware("admin"),wrapAsync(allBooking));
+router.route("/") 
+.get(isLogin,roleMiddleware("admin"),wrapAsync(allBooking));
+ router.get("/userBooking",isLogin,checkUser,wrapAsync(getUserBookings))
+ router.get("/carBooking",isLogin,checkCar,wrapAsync(allBookofCar))
+router.route("/:id")
+.get(isLogin,checkCar,wrapAsync(getBookingOfToday))
+.post(isLogin,checkCar, checkUser,wrapAsync(addUserBooking))
+.patch(isLogin,checkUser,wrapAsync(updateBooking))
+.delete(isLogin,checkUser,wrapAsync(deleteBooking ))
 
-export default router;
+//add validate in post and patch
+
+export default router; 
