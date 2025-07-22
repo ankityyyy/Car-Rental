beba4e82a654f095816ed0ec6c2b8bdb70f1de01
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-22 19:31:59 +0530

    remove the couponUsed

--- backend/src/models/bookingSchema.js
+++ backend/src/models/bookingSchema.js
@@ -1,111 +1,84 @@
 import mongoose from "mongoose";
 
-const bookingSchema=new mongoose.Schema({
-     userId:{
-          type:mongoose.Schema.Types.ObjectId,
-          ref:"User",
-            required: true
+const bookingSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
   carId: {
-  type: mongoose.Schema.Types.ObjectId, 
-  ref: "Car",
-  required: true
-},
-
-  startDate:{
-   type: Date,
-     required:true,
-
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Car",
+    required: true,
   },
-  endDate:{
+  paymentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Payment",
+  },
+  startDate: {
     type: Date,
-    
+    required: true,
   },
-  totalDays:{
-      type:Number,
-    
-  },
-  pickupTime:{
+  endDate: {
     type: Date,
-     required:true,
   },
-  dropoffTime:{
-    type: Date 
-
+  totalDays: {
+    type: Number,
   },
-  pickupLocation:{
-     type:String,
-     required:true
-
+  pickupTime: {
+    type: Date,
+    required: true,
   },
-  dropoLocation:{
-      type:String,
-     required:true
-
-
+  dropoffTime: {
+    type: Date,
   },
-  startKm:{
-     type:Number,
-     required:true,
-     default:0,
-
+  pickupLocation: {
+    type: String,
+    required: true,
   },
-  actualKm:{
-      type:Number,
-
+  dropoLocation: {
+    type: String,
+    required: true,
+  },
+  startKm: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  actualKm: {
+    type: Number,
   },
 
   pricingType: {
     type: String,
     enum: ["perDay", "perKm", "custom"],
-    required: true
+    required: true,
   },
-  couponUsed: {
-  type: String, 
-  default: null
-},
 
+  
   priceBeforeDiscount: {
     type: Number,
-    required: true
+    required: true,
   },
   discountAmount: {
     type: Number,
-    default: 0
+    default: 0,
   },
   walletUsed: {
     type: Number,
-    default: 0
+    default: 0,
   },
-  
-
   finalPrice: {
     type: Number,
-    required: true
+    required: true,
   },
-
-  paymentStatus: {
-    type: String,
-    enum: ["pending", "paid", "failed"],
-    default: "pending"
-  },
-  paymentId: {
-  type: String 
-},
-
-invoiceUrl: {
-  type: String 
-},
-
 
   bookingStatus: {
-  type: String,
-  enum: ["pending", "confirmed", "completed", "cancelled"],
-  default: "pending"
-},
-     
-},{ timestamps: true })
+    type: String,
+    enum: ["pending", "confirmed", "completed", "cancelled"],
+    default: "pending",
+  },
+}, { timestamps: true });
 
-const booking=mongoose.model("Booking",bookingSchema);
-
+const booking = mongoose.model("Booking", bookingSchema);
 export default booking;
