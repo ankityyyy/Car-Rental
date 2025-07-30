e62796e2901278595cf53c1b7c9cc9cc30dce608
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-30 16:39:37 +0530

    add image update routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,12 +1,18 @@
 import express from "express";
 const router=express.Router();
-import { registerUser,loginUser } from "../controller/user.js";
+import { registerUser,loginUser,addProfileImage } from "../controller/user.js";
 import wrapAsync from "../utils/wrapAsync.js"
 import validate from "../middleware/userMiddleware.js";
 import {userSchemaReigster,userSchemaLogin } from "../joiSchema/user.js"
+import {isLogin} from "../middleware/isLogin.js"
+import multer from "multer";
+import {storage} from "../config/cloudinary.js";
+const upload = multer({ storage});
 
 
 router.post("/register",validate(userSchemaReigster),wrapAsync(registerUser))
 router.post("/login",validate(userSchemaLogin),wrapAsync(loginUser));
+ router.post("/addProfile",upload.single("image"), isLogin, wrapAsync(addProfileImage));
+ 
 
 export default router; 
