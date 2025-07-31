df8b9d7dd889c25c32b7996377554a9f59615050
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-31 17:26:09 +0530

    add required in phone number based on logic

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -16,16 +16,20 @@ const userSchema = new mongoose.Schema({
     trim: true
   },
   phone: {
-    type: String,
-    unique: true,
-    validate: {
-      validator: function (v) {
-        return /^\d{10}$/.test(v); 
-      },
-      message: (props) =>
-        `${props.value} is not a valid 10-digit phone number!`
-    }
+  type: String,
+  unique: true,
+  validate: {
+    validator: function (v) {
+      if (!v && this.provider === "google") return true; 
+      return /^\d{10}$/.test(v); 
+    },
+    message: (props) =>
+      `${props.value} is not a valid 10-digit phone number!`
   },
+  required: function () {
+    return this.provider === "local";
+  }
+},
   provider: {
     type: String,
     default: "local"
