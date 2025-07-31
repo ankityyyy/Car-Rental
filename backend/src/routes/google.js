7c019147780e595e198558808b38034530600718
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-31 17:16:37 +0530

    check google route is work or not

--- backend/src/routes/google.js
+++ backend/src/routes/google.js
@@ -8,6 +8,10 @@ router.get("/google", passport.authenticate("google", {
   scope: ["profile", "email"]
 }));
 
+
+
+
+
 router.get("/google/callback",
   passport.authenticate("google", { session: false, failureRedirect: "/login-failure" }),
   generateTokenAndRedirect
